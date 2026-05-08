c086ca798735aface7e6bc29005eb0be2e4d1622
vajhxajhcv <13617131629@163.com>
2026-05-08 15:44:30 +0800

    rename site to wuming nursery

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '无名苗圃';
+export const SITE_DESCRIPTION = '一个存放记忆与故事的地方';
